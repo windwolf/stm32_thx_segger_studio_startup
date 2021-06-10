739e6e9914937258531580a16f3a40bf45f01e39
windwolf <zhoujian.ww@gmail.com>
2021-06-10 16:58:25 +0800

    为兴伴device而重构

--- src/App/demo.c
+++ src/App/demo.c
@@ -3,35 +3,41 @@
 #include "basic/ring_buffer8.h"
 #include "mem_layout.h"
 #include "basic/stream.h"
-#include "basic/command.h"
-#include "basic/pin.h"
-#include "stream_io_device_uart.h"
-#include "packet_io_device_spi.h"
-#include "pin_gpio.h"
+#include "basic/five_step_command_client.h"
+#include "basic/device.h"
+#include "bsp.h"
 #include "stm32h7xx_ll_spi.h"
 #include "st77xx/st7735.h"
 #include "w25qxx/w25qxx_spi.h"
 #include "tracex.h"
-#define W25QXX_BUFFER_SIZE 2048
-extern SPI_HandleTypeDef hspi4;
-Pin csPin;
-Pin dcPin;
-D2_BUFFER PacketIoDevice spi4_pio;
-D2_BUFFER ST77XX st7735;
 
 extern UART_HandleTypeDef huart4;
+D2_BUFFER UartDevice uartDevice;
 D2_BUFFER uint8_t txBuf0[64];
-D2_BUFFER uint8_t testRxBuf[65];
-D2_BUFFER RingBuffer8 buffer2;
+D2_BUFFER uint8_t uartRxBuffer[65];
+D2_BUFFER RingBuffer uartRxRingBuffer;
 D2_BUFFER Stream stream;
+
+extern SPI_HandleTypeDef hspi4;
+PinDevice csPin;
+PinDevice dcPin;
+D2_BUFFER SpiDevice spi4Dev;
+D2_BUFFER SpiWithPinsDevice spi4pDev;
+D2_BUFFER FiveStepCommandCient st7735_cmd;
+#define ST7735_BUFFER_SIZE 48114
+D2_BUFFER uint8_t st7735Buffer[ST7735_BUFFER_SIZE];
+D2_BUFFER ST77XX st7735;
 
 extern QSPI_HandleTypeDef hqspi;
 
+#define W25QXX_BUFFER_SIZE 2048
+D2_DATA uint32_t w25qxx_1_id;
+D1_BUFFER uint8_t w25qxx_data_buf[500];
 extern SPI_HandleTypeDef hspi1;
-Pin csPin_1;
-D2_BUFFER PacketIoDevice spi1_pio;
-D2_BUFFER SimpleCommand w25qxx_cmd;
-
+D2_BUFFER PinDevice csPin_1;
+D2_BUFFER SpiDevice spi1Dev;
+D2_BUFFER SpiWithPinsDevice spi1pDev;
+D2_BUFFER FiveStepCommandCient w25qxx_cmd;
 D2_BUFFER uint8_t w25qxx_1_buf1[W25QXX_BUFFER_SIZE];
 D2_BUFFER W25QXX_SPI w25qxx_1;
 
@@ -50,42 +56,31 @@ ULONG thread_1_counter;
 
 void init_driver()
 {
-    RingBuffer8_Create(&buffer2, testRxBuf, 65);
+    ringbuffer_create(&uartRxRingBuffer, uartRxBuffer, 65);
+    uart_device_create(&uartDevice, &huart4, 0);
+    stream_create(&stream, &uartDevice, &uartRxRingBuffer);
+    stream_server_start(&stream);
 
-    Uart_StreamDevice_Create((StreamIoDevice *)&stream, &huart4);
-    Stream_Init(&stream, &buffer2);
-    Stream_StartServer(&stream);
+    pin_device_create(&dcPin, GPIOE, GPIO_PIN_13, PIN_DEVICE_STATUS_INVERSE_NORMAL);
+    spi_device_create(&spi4Dev, &hspi4, 4);
+    spi_with_pins_device_create(&spi4pDev, &spi4Dev, NULL, NULL, &dcPin);
+    five_step_command_client_create(&st7735_cmd, &spi4pDev);
+    Buffer buf1 = {.data = st7735Buffer, .size = ST7735_BUFFER_SIZE};
+    st7735_create(&st7735, &st7735_cmd, buf1);
 
-    // Gpio_PinDevice_Create((PinDevice *)&csPin, GPIOE);
-    // Pin_Init(&csPin);
-    // csPin.pinMask = GPIO_PIN_11;
-
-    Gpio_PinDevice_Create((PinDevice *)&dcPin, GPIOE);
-    Pin_Init(&dcPin);
-    dcPin.pinMask = GPIO_PIN_13;
-
-    spi_packet_io_device_create(&spi4_pio, &hspi4);
-    simple_command_create(&st7735.command, &spi4_pio,
-                          NULL, NULL,
-                          NULL, NULL,
-                          &dcPin, COMMAND_DATACMD_PIN_MODE_DATA);
-
-    ST7735_Init(&st7735);
-
-    spi_packet_io_device_create(&spi1_pio, &hspi1);
-    Gpio_PinDevice_Create((PinDevice *)&csPin_1, GPIOD);
-    Pin_Init(&csPin_1);
-    csPin_1.pinMask = GPIO_PIN_6;
-    simple_command_create(&w25qxx_cmd, &spi1_pio, &csPin_1, COMMAND_SELECT_PIN_MODE_UNSELECT, NULL, NULL, NULL, NULL);
-    Buffer buf = {.data = w25qxx_1_buf1, .size = W25QXX_BUFFER_SIZE};
-    w25qxx_spi_create(&w25qxx_1, buf, &w25qxx_cmd);
+    pin_device_create(&csPin_1, GPIOD, GPIO_PIN_6, PIN_DEVICE_STATUS_INVERSE_NORMAL);
+    spi_device_create(&spi1Dev, &hspi1, 4);
+    spi_with_pins_device_create(&spi1pDev, &spi1Dev, &csPin_1, NULL, NULL);
+    five_step_command_client_create(&w25qxx_cmd, &spi1pDev);
+    Buffer buf2 = {.data = w25qxx_1_buf1, .size = W25QXX_BUFFER_SIZE};
+    w25qxx_spi_create(&w25qxx_1, buf2, &w25qxx_cmd);
 }
 
 void tx_application_define(void *first_unused_memory)
 {
     TraceX_EnableTrace();
     init_driver();
-    // UartDevice_Init(&uart, &huart4, &buffer2);
+    // UartDevice_Init(&uart, &huart4, &uartRxRingBuffer);
     // UartDevice_StartServer(&uart);
 
     CHAR *pointer = NULL;
@@ -130,8 +125,8 @@ void test05_init()
     st7735.height = 80;
     st7735.colorMode = ST7735_COLOR_MODE_16BIT;
     st7735.orientation = ST7735_DISPLAY_DIRECTION_XY_EXCHANGE_Y_MIRROR | ST7735_DISPLAY_COLOR_DIRECTION_BGR | ST7735_DISPLAY_REFRESH_ORDER_T2B_L2R;
-    ST7735_Reset(&st7735);
-    //ST7735_Inversion(&st7735, 1);
+    st7735_reset(&st7735);
+    //st77xx_inversion(&st7735, 1);
     for (size_t i = 0; i < LCD_DATA_SIZE; i++)
     {
         lcddata[i] = (0xF800);
@@ -144,10 +139,10 @@ void test05()
     uint16_t color1 = 0x001F;
     uint16_t color2 = 0xF800;
     uint16_t color3 = 0x04F1;
-    ST7735_DrawRect(&st7735, 0, 0, st7735.width, st7735.height, color0); //inv:1=red; inv:0=yellow
-    ST7735_DrawHLine(&st7735, 10, 10, 20, color1);                       //inv:1=red+green; inv:0=sky+pink
-    ST7735_DrawRect(&st7735, 20, 20, 10, 10, color2);                    //inv:1=blue; inv:0=sky
-    ST7735_DrawRect(&st7735, 40, 20, 10, 10, color3);                    //inv:1=red; inv:0=yellow
+    st7735_rect_draw(&st7735, 0, 0, st7735.width, st7735.height, color0); //inv:1=red; inv:0=yellow
+    st7735_hline_draw(&st7735, 10, 10, 20, color1);                       //inv:1=red+green; inv:0=sky+pink
+    st7735_rect_draw(&st7735, 20, 20, 10, 10, color2);                    //inv:1=blue; inv:0=sky
+    st7735_rect_draw(&st7735, 40, 20, 10, 10, color3);                    //inv:1=red; inv:0=yellow
 }
 
 void thread_0_entry(ULONG thread_input)
@@ -156,8 +151,8 @@ void thread_0_entry(ULONG thread_input)
     //UINT status;
     // UartDevice_Tx(&uart, (uint8_t *)text, strlen(text));
     // UartDevice_WaitForTxComplete(&uart, TX_WAIT_FOREVER);
-    Stream_Tx(&stream, (uint8_t *)text, strlen(text));
-    Stream_WaitForTxComplete(&stream, TX_WAIT_FOREVER);
+    stream_send(&stream, (uint8_t *)text, strlen(text));
+    stream_send_cplt_wait(&stream, TX_WAIT_FOREVER);
 
     test05_init();
     /* This thread simply sits in while-forever-sleep loop.  */
@@ -180,8 +175,6 @@ void thread_0_entry(ULONG thread_input)
     }
 }
 
-D2_DATA uint32_t w25qxx_1_id;
-D1_BUFFER uint8_t w25qxx_data_buf[500];
 void thread_1_entry(ULONG thread_input)
 {
 
@@ -191,7 +184,7 @@ void thread_1_entry(ULONG thread_input)
     LOG("W25QXX: id=%d", w25qxx_1_id);
     w25qxx_spi_status_get(&w25qxx_1);
     LOG("W25QXX: s1=%d, s2=%d, s3=%d", w25qxx_1.base.status1, w25qxx_1.base.status2, w25qxx_1.base.status3);
-    
+
     w25qxx_1_id++;
     LOG("W25QXX: w=%d", w25qxx_1_id);
     w25qxx_spi_block_erase(&w25qxx_1, 0x0000);
@@ -201,16 +194,16 @@ void thread_1_entry(ULONG thread_input)
     /* This thread simply sends messages to a queue shared by thread 2.  */
     while (1)
     {
-        Stream_WaitForRxReady(&stream, TX_WAIT_FOREVER);
-        uint32_t len = RingBuffer8_GetCount(&buffer2);
+        stream_receive_ready_wait(&stream, TX_WAIT_FOREVER);
+        uint32_t len = ringbuffer_count_get(&uartRxRingBuffer);
         if (len > 0)
         {
 
-            RingBuffer8_Read(&buffer2, txBuf0, len);
-            Stream_Tx(&stream, txBuf0, len);
+            ringbuffer_read(&uartRxRingBuffer, txBuf0, len);
+            stream_send(&stream, txBuf0, len);
             w25qxx_spi_write(&w25qxx_1, txBuf0, 0x0400, len);
             w25qxx_spi_read(&w25qxx_1, w25qxx_data_buf, 0x0400, len);
-            LOG("W25QXX: ri=%s", *((uint32_t *)w25qxx_data_buf));
+            LOG("W25QXX: ri=%d", *((uint32_t *)w25qxx_data_buf));
             tx_thread_sleep(10);
             //cRead++;
         }
