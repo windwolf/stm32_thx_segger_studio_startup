cea8e238bf127316befa968d60afab9764e2002e
windwolf <zhoujian.ww@gmail.com>
2021-07-20 10:55:03 +0800

    消除若干警告

--- src/test/unit_test.c
+++ src/test/unit_test.c
@@ -1,8 +1,23 @@
 #include "unit_test.h"
 #include "communication_protocol/inc/message_parser_test.h"
+#include "stdint.h"
+#include "stdio.h"
+#define LOG_MODULE test
+#include "log.h"
+
+typedef struct {
+	uint8_t a;
+	uint32_t b;
+} __attribute__((packed)) A;
+typedef struct {
+	uint8_t a;
+	uint32_t b;
+} B __attribute__((packed)) ;
 
 void test_before_hal_init()
 {
+	printf("sizeof(A):%d", sizeof(A));
+	printf("sizeof(B):%d", sizeof(B));
     message_parser_test();
 }
 
