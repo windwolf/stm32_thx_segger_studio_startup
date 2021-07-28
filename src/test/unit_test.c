7aa30db42391ec9b875d1662524740c7249cb87e
windwolf <zhoujian.ww@gmail.com>
2021-07-28 18:42:39 +0800

    调整 单元测试方法

--- src/test/unit_test.c
+++ src/test/unit_test.c
@@ -1,7 +1,7 @@
 #include "unit_test.h"
 #include "communication_protocol/inc/message_parser_test.h"
 #include "communication_protocol/inc/nmea_test.h"
-#include "communication_protocol/inc/console_test.h"
+#include "communication_protocol/inc/tree_accessor_test.h"
 #include "stdint.h"
 #include "stdio.h"
 #define LOG_MODULE test
@@ -24,7 +24,7 @@ void test_before_hal_init()
 	printf("sizeof(A):%d", sizeof(A));
 	//printf("sizeof(B):%d", sizeof(B));
 	message_parser_test();
-	console_test();
+	tree_accessor_test();
 }
 
 void test_after_hal_init()
