70fb9e10d83b507e19cefdeb6f6a9b8cc7bc4d83
windwolf <zhoujian.ww@gmail.com>
2021-08-05 20:33:56 +0800

    增加卡尔曼滤波的配套测试

--- src/test/unit_test.c
+++ src/test/unit_test.c
@@ -2,6 +2,8 @@
 #include "communication_protocol/inc/message_parser_test.h"
 #include "communication_protocol/inc/nmea_test.h"
 #include "communication_protocol/inc/tree_accessor_test.h"
+#include "algorithm/inc/kf_test.h"
+#include "algorithm/inc/pid_test.h"
 #include "stdint.h"
 #include "stdio.h"
 #define LOG_MODULE test
@@ -13,6 +15,7 @@ typedef struct
 	uint8_t a;
 	uint32_t b;
 } __attribute__((packed)) A;
+
 //typedef struct
 //{
 //	uint8_t a;
@@ -23,6 +26,7 @@ void test_before_hal_init()
 {
 	//printf("sizeof(A):%d", sizeof(A));
 	//printf("sizeof(B):%d", sizeof(B));
+	kf_test();
 	message_parser_test();
 	tree_accessor_test();
 }
